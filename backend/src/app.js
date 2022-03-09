0668aad546544489a709f2c6d66408f7443bf3eb
io-panic <madox911@aol.com>
2022-03-08 23:01:08 -0500

    Use directory aliases + Dockerfile

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,22 +3,21 @@ import favicon from "serve-favicon";
 import compress from "compression";
 import helmet from "helmet";
 import cors from "cors";
-import logger from "./logger.js";
+import logger from "@/logger.js";
 
 import feathers from "@feathersjs/feathers";
 import configuration from "@feathersjs/configuration";
 import express from "@feathersjs/express";
 
-import middleware from "./middleware/index.js";
-import services from "./services/index.js";
-import { before, after, error } from "./app.hooks.js";
-import channels from "./channels.js";
+import middleware from "@/middleware/index.js";
+import services from "@/services/index.js";
+import { before, after, error } from "@/app.hooks.js";
+import channels from "@/channels.js";
 
 const app = express(feathers());
 
-// Load app configuration
 app.configure(configuration());
-// Enable security, CORS, compression, favicon and body parsing
+
 app.use(
   helmet({
     contentSecurityPolicy: false,
@@ -30,20 +29,14 @@ app.use(compress());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(favicon(join(app.get("public"), "favicon.ico")));
-// Host the public folder
+
 app.use("/", express.static(app.get("public")));
 
-// Set up Plugins and providers
 app.configure(express.rest());
-
-// Configure other middleware (see `middleware/index.js`)
 app.configure(middleware);
-// Set up our services (see `services/index.js`)
 app.configure(services);
-// Set up event channels (see channels.js)
 app.configure(channels);
 
-// Configure a middleware for 404s and the error handler
 app.use(express.notFound());
 app.use(express.errorHandler({ logger }));
 
--- backend/src/services/index.js
+++ backend/src/services/index.js
@@ -1,6 +1,5 @@
-import events from "./events/events.service.js";
+import events from "@/services/events/events.service.js";
 
-// eslint-disable-next-line no-unused-vars
 export default function (app) {
   app.configure(events);
 }
